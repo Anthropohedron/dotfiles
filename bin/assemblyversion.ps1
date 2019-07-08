0bcb321c29b1c1d0bc0b1d44ef5c96f455d0a40c
Anthropohedron <Anthropohedron@users.noreply.github.com>
2019-07-08 13:58:42 -0400

    added a WithDependencies switch

--- bin/assemblyversion.ps1
+++ bin/assemblyversion.ps1
@@ -1,8 +1,13 @@
 #!/usr/bin/env powershell
 param (
-	[string]$file
+	[string]$file,
+	[switch]$WithDependencies
 )
 $assembly = [System.Reflection.Assembly]::ReflectionOnlyLoadFrom($file)
 $version = $assembly.GetName().Version.ToString()
 
 echo "$file version is $version"
+
+if ($WithDependencies) {
+	$assembly.GetReferencedAssemblies() | Format-Table -AutoSize
+}
