6ce30467775eeb41721cdaa07b5144c20faead62
Anthropohedron <Anthropohedron@users.noreply.github.com>
2020-05-31 17:48:13 -0400

    increased flexibility in opening URLs

--- .platform/kernel/Cygwin/bin/OpenUrl.ps1
+++ .platform/kernel/Cygwin/bin/OpenUrl.ps1
@@ -3,8 +3,12 @@
 $clip = Get-Clipboard -Format Text -Raw
 $url = [uri]$clip
 
+if (($url.AbsoluteUri -eq $null) -and -not $clip.Contains("://")) {
+	$url = [uri]"https://$clip"
+}
+
 if ($url.AbsoluteUri -ne $null) {
-	Start-Process "$clip"
+	Start-Process $url
 } else {
 	Write-Warning "Bad Url '$clip'"
 	pause
