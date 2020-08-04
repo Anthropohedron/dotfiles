fad4c48e225f93e29ae7539969101e6dbe24bfd9
Anthropohedron <Anthropohedron@users.noreply.github.com>
2020-08-04 16:24:42 -0400

    improved OpenUrl.ps1 to check the X11 selection if the clipboard has garbage and X11 is available

--- .platform/kernel/Cygwin/bin/OpenUrl.ps1
+++ .platform/kernel/Cygwin/bin/OpenUrl.ps1
@@ -1,16 +1,48 @@
 #!/usr/bin/env pwsh
 
-$clip = Get-Clipboard -Format Text -Raw
-$url = [uri]$clip
+function UrlFromString($maybeUrlString) {
+	$maybeUrl = [uri]$maybeUrlString
 
-if (($url.AbsoluteUri -eq $null) -and -not $clip.Contains("://")) {
-	$url = [uri]"https://$clip"
+	if (($maybeUrl.AbsoluteUri -eq $null) -and `
+	     -not $maybeUrlString.Contains("://")) {
+		$maybeUrl = [uri]"https://$maybeUrlString"
+	}
+	if ($maybeUrl.AbsoluteUri -ne $null) {
+		return $maybeUrl
+	} else {
+		Write-Warning "Bad Url '$maybeUrlString"
+		return $null
+	}
 }
 
-if ($url.AbsoluteUri -ne $null) {
-	Start-Process $url
-} else {
-	Write-Warning "Bad Url '$clip'"
+function Die() {
 	pause
 	exit 1
 }
+
+$clip = Get-Clipboard -Format Text -Raw
+$url = UrlFromString($clip)
+
+if ($url -eq $null) {
+	Write-Warning "Bad Url '$clip'"
+	$cygbin = "/cygwin/bin"
+	if (-not (Test-Path $cygbin -PathType Container)) {
+		$cygbin = "/cygwin64/bin"
+		if (-not (Test-Path $cygbin -PathType Container)) {
+			Die
+		}
+	}
+	$xsel = "$cygbin/xsel.exe"
+	$sh = "$cygbin/sh.exe"
+	if ((Test-Path $xsel -Type Leaf) -and (Test-Path $sh -Type Leaf)) {
+		$clip = & $sh "-c" "PATH=/bin DISPLAY=:0 xsel 2>/dev/null"
+		$url = UrlFromString($clip)
+	}
+}
+
+
+if ($url -eq $null) {
+	Die
+}
+
+Start-Process $url
