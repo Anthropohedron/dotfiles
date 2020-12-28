797546956e7ceb07ffbda8aefddcb8cff49fa782
Anthropohedron <Anthropohedron@users.noreply.github.com>
2020-12-28 15:42:50 -0500

    still more PowerShell config

--- .platform/kernel/Cygwin/pwshrc.ps1
+++ .platform/kernel/Cygwin/pwshrc.ps1
@@ -1,3 +1,4 @@
+# vim: ts=4 sw=4 noexpandtab
 # To automatically load, run the following in PowerShell once:
 # Set-Content $PROFILE ". $HOME\.platform\kernel\Cygwin\pwshrc.ps1"
 
@@ -6,5 +7,12 @@ Set-Alias -Name:"title" -Value:"Set-Title" -Description:"" -Option:"None"
 
 if ($host.Name -eq 'ConsoleHost')
 {
-				Import-Module -ErrorAction SilentlyContinue PSReadLine
+	if (-not (Get-Module -ListAvailable -Name PSReadLine))
+	{
+		Install-Module PSReadLine -Scope CurrentUser -Force
+	}
+	Import-Module PSReadLine
+	Set-PSReadLineOption -EditMode Emacs
+	Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+	Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 }
