cbb77532cca7798cac7f3206e9389152536d8f8c
Anthropohedron <Anthropohedron@users.noreply.github.com>
2020-12-28 15:23:49 -0500

    more PowerShell config

--- .platform/kernel/Cygwin/pwshrc.ps1
+++ .platform/kernel/Cygwin/pwshrc.ps1
@@ -1,3 +1,10 @@
+# To automatically load, run the following in PowerShell once:
+# Set-Content $PROFILE ". $HOME\.platform\kernel\Cygwin\pwshrc.ps1"
+
 function Set-Title([string]$title) { $host.ui.RawUI.WindowTitle = $title }
 Set-Alias -Name:"title" -Value:"Set-Title" -Description:"" -Option:"None"
 
+if ($host.Name -eq 'ConsoleHost')
+{
+				Import-Module -ErrorAction SilentlyContinue PSReadLine
+}
