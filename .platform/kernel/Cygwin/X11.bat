e8d9ab6a357cb8a1b133da1f3269337de8813bb9
Anthropohedron <Anthropohedron@users.noreply.github.com>
2020-12-29 13:09:32 -0500

    fixed up X11 stuff

--- .platform/kernel/Cygwin/X11.bat
+++ .platform/kernel/Cygwin/X11.bat
@@ -4,7 +4,7 @@ if exist C:\cygwin set CYGDIR=C:\cygwin
 if exist C:\cygwin64 set CYGDIR=C:\cygwin64
 if "%CYGDIR%"=="" goto fail
 
-%CYGDIR%\bin\startxwin.exe -multiwindow -clipboard -emulate3buttons -nounixkill -nowinkill
+%CYGDIR%\bin\sh.exe "PATH=/bin:/usr/bin /bin/startxwin -multiwindow -clipboard -emulate3buttons -nounixkill -nowinkill"
 
 goto end
 
