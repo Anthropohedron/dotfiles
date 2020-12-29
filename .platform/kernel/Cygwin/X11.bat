301c20fd152c81d03aa4a9515751995f6247b319
Anthropohedron <Anthropohedron@users.noreply.github.com>
2020-12-29 12:47:11 -0500

    do better with starting X11, especially on Windows

--- .platform/kernel/Cygwin/X11.bat
+++ .platform/kernel/Cygwin/X11.bat
@@ -4,7 +4,7 @@ if exist C:\cygwin set CYGDIR=C:\cygwin
 if exist C:\cygwin64 set CYGDIR=C:\cygwin64
 if "%CYGDIR%"=="" goto fail
 
-%CYGDIR%\bin\run.exe -p /usr/bin -p /bin Xwin -multiwindow -clipboard -emulate3buttons -nounixkill -nowinkill
+%CYGDIR%\bin\startxwin.exe -multiwindow -clipboard -emulate3buttons -nounixkill -nowinkill
 
 goto end
 
