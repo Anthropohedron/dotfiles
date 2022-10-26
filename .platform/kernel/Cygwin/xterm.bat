17c516306f0b326cd697ac2c5516c9bec35ca4d7
Anthropohedron <Anthropohedron@users.noreply.github.com>
2022-10-26 16:30:12 -0400

    bugfix

--- .platform/kernel/Cygwin/xterm.bat
+++ .platform/kernel/Cygwin/xterm.bat
@@ -4,7 +4,7 @@ if exist C:\cygwin set CYGDIR=C:\cygwin
 if exist C:\cygwin64 set CYGDIR=C:\cygwin64
 if "%CYGDIR%"=="" goto fail
 
-cd /D %HOME%
+cd /D %USERPROFILE%
 %CYGDIR%\bin\run.exe -p /usr/bin -p /bin xterm -display :0 -ls zsh
 
 goto end
